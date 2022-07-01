534d81b1ca9894b49bba6bf46580a7c1442e7f10
FutureAxolotl <100148624+FutureAxolotl@users.noreply.github.com>
2022-07-01 16:16:45 -0400

    Fixed launch.sh to work with polywins

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env sh
-
-# Terminate already running bar instances
-killall -q polybar
-
-# Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
-
-# Launch
-polybar bar1 &
+if type "xrandr" >/dev/null; then
+    for m in $(polybar -m | cut -d':' -f1); do
+        MONITOR=$m polybar --reload bar1 &
+    done
+else
+    polybar --reload bar1 &
+fi
