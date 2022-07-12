50a16473b3370e098690a7919daabc407c3b827d
FutureAxolotl <100148624+FutureAxolotl@users.noreply.github.com>
2022-07-12 15:37:36 -0400

    Fixed Fish colors

--- fish/config.fish
+++ fish/config.fish
@@ -12,4 +12,18 @@ macchina
 alias sudo "doas"
 alias sudoedit='doas rnano'
 end
-fish_add_path /home/future_axolotl/.spicetify
+
+#colors
+set fish_color_normal dedede
+set fish_color_command 7aa6da
+set fish_color_quote e78c45
+set fish_color_redirection c397d8
+set fish_color_end c397d8
+set fish_color_error d54e53
+set fish_color_param ffffff
+set fish_color_comment 999999
+set fish_color_match 70c0b1
+set fish_color_search_match c397d8
+set fish_color_operator 70c0b1
+set fish_color_escape 70c0b1
+set fish_color_cwd b9ca4a
